f980c6e3a048893d82c0bb446803768c2d9b42e4
weqopy <weqopy@gmail.com>
2018-08-07 23:55:09 +0800

    add an eg

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -22,3 +22,54 @@ class SnippetSerializer(serializers.Serializer):
         instance.style = validated_data.get("style", instance.style)
         instance.save()
         return instance
+
+
+# ---------- 示例 ----------
+#
+"""
+python manage.py shell 进入 Django shell
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+snippet = Snippet(code='foo = "bar"\n')
+snippet.save()
+
+snippet = Snippet(code='print "hello, world"\n')
+snippet.save()
+
+snippet = Snippet(code='print "hello, the third snippet"\n')
+serializer.save()
+
+# 实例序列化
+# 模型实例转换为 Python 原生数据类型
+serializer = SnippetSerializer(snippet)
+serializer.data
+# {'id': 3, 'title': '', 'code': 'print "hello, the third snippet"\n', 'linenos': False, 'language': 'python', 'style': 'friendly'}
+# 完成序列化，将数据转换为 json
+content = JSONRenderer().render(serializer.data)
+content
+# b'{"id":3,"title":"","code":"print \\"hello, the third snippet\\"\\n","linenos":false,"language":"python","style":"friendly"}'
+
+# 反序列化
+# 将一个流解析为 Python 原生数据类型
+from django.utils.six import BytesIO
+
+stream = BytesIO(content)
+data = JSONParser().parse(stream)
+# 将 Python 原生数据类型恢复为正常的对象实例
+serializer = SnippetSerializer(data=data)
+serializer.is_valid()
+# True
+serializer.validated_data
+# OrderedDict([('title', ''), ('code', 'print "hello, the third snippet"'), ('linenos', False), ('language', 'python'), ('style', 'friendly')])
+serializer.save()
+# <Snippet: Snippet object (4)>
+
+# 序列化查询结果集（querysets），只需添加`many=True`
+serializer = SnippetSerializer(Snippet.objects.all(), many=True)
+serializer.data
+# [OrderedDict([('id', 1), ('title', ''), ('code', 'foo = "bar"\n'), ('linenos', False), ('language', 'python'), ('style', 'friendly')]), OrderedDict([('id', 2), ('title', ''), ('code', 'print "hello, world"\n'), ('linenos', False), ('language', 'python'), ('style', 'friendly')]), OrderedDict([('id', 3), ('title', ''), ('code', 'print "hello, the third snippet"\n'), ('linenos', False), ('language', 'python'), ('style', 'friendly')]), OrderedDict([('id', 4), ('title', ''), ('code', 'print "hello, the third snippet"'), ('linenos', False), ('language', 'python'), ('style', 'friendly')])]
+"""
