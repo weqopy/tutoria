8d953d8134c339778f3aebefd203f535f08ef612
weqopy <weqopy@gmail.com>
2018-08-08 11:05:27 +0800

    use mixed-in views

--- snippets/views.py
+++ snippets/views.py
@@ -5,42 +5,56 @@ from rest_framework.response import Response
 from rest_framework import status, mixins, generics
 from django.http import Http404
 
+"""
+使用 mixed-in 通用视图
+"""
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
 
 """
 使用 mixin 类编写视图，使代码更简洁
 """
 
 
-class SnippetList(
-    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
-):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+# class SnippetList(
+#     mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
+# ):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
 
-class SnippetDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView,
-):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+# class SnippetDetail(
+#     mixins.RetrieveModelMixin,
+#     mixins.UpdateModelMixin,
+#     mixins.DestroyModelMixin,
+#     generics.GenericAPIView,
+# ):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(self, request, *args, **kwargs)
+#     def put(self, request, *args, **kwargs):
+#         return self.update(self, request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(self, request, *args, **kwargs)
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(self, request, *args, **kwargs)
 
 
 """
