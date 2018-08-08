720b4c6809afd972f74d7eef925bd04769fe41be
weqopy <weqopy@gmail.com>
2018-08-08 10:25:39 +0800

    add format=None in views, return a much useful api on browser

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,8 +1,11 @@
 from snippets import views
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path("", views.snippet_list),
     # 后加 /， 否则被识别为 snippets/ <int:pk>
     path("<int:pk>/", views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -7,11 +7,14 @@ from rest_framework import status
 """
 使用基于函数视图的@api_view 装饰器，确保获得 Request 对象，
 核心是可以处理任意数据的 request.data 属性
+
+view 函数增加 format=None 参数，使用浏览器访问默认返回 HTML 格式表示，
+有更大的可用性。
 """
 
 
 @api_view(["GET", "POST"])
-def snippet_list(request):
+def snippet_list(request, format=None):
     # 列出所有的 code snippet，或创建一个新的 snippet
     if request.method == "GET":
         snippets = Snippet.objects.all()
@@ -27,7 +30,7 @@ def snippet_list(request):
 
 
 @api_view(["GET", "POST", "DELETE"])
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
     # 获取、更新或删除应该 code snippet
     try:
         snippet = Snippet.objects.get(pk=pk)
